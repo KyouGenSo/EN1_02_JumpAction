ed1fba34ab2cf29abbb01342c5e9a7ca188e2802
KyouGenSo <k023g0083@g.neec.ac.jp>
2024-06-03 12:40:32 +0900

    第7回_01完成

--- Assets/Scripts/PullingJump.cs
+++ Assets/Scripts/PullingJump.cs
@@ -8,6 +8,16 @@ public class PullingJump : MonoBehaviour
     private Vector3 clickPos;
     [SerializeField] 
     private float jumpForce = 10.0f;
+    bool isJumping = false;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isJumping = false;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isJumping = true;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +33,7 @@ public class PullingJump : MonoBehaviour
             clickPos = Input.mousePosition;
         }
 
-        if(Input.GetMouseButtonUp(0))
+        if(Input.GetMouseButtonUp(0) && !isJumping)
         {
             Vector3 dist = clickPos - Input.mousePosition;
             if(dist.sqrMagnitude == 0) { return; }
